162ed7253dc3404b3783171fabfd5bf3a8b16e60
Marcos Gomez <mdgdevelop@gmail.com>
2020-03-07 12:37:39 -0500

    fixed photo layout

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,102 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container">
+		<div className="text-center">
+			<h1>AnmaProductions</h1>
+			<a href="https://www.instagram.com/anmaproductions/">
+				<p>Instagram</p>
+			</a>
+		</div>
+		<div className="card-columns">
+			<div className="card">
+				<img
+					src="https://emilia.lekoarts.de/static/a40e525ac7c6809bb4237eea5a758e36/0797f/Reflections.webp"
+					className="card-img-top"
+					alt="..."
+				/>
+			</div>
+			<div className="card">
+				<img
+					src="https://emilia.lekoarts.de/static/a40e525ac7c6809bb4237eea5a758e36/0797f/Reflections.webp"
+					className="card-img-top"
+					alt="..."
+				/>
+			</div>
+
+			<img
+				src="https://emilia.lekoarts.de/static/7c1277b9a6c75d059cfa9245aec8a206/f043f/grant-ritchie-wDOeiVmERPI-unsplash.webp"
+				className="card-img-top"
+				alt="..."
+			/>
+
+			<div className="card mt-4 card1">
+				<img
+					src="https://emilia.lekoarts.de/static/8ba4212c3d1020107ae2ae40ef574c4b/80926/di_an_h-9jsV5uKbAEM-unsplash.webp"
+					className="card-img-top"
+					alt="..."
+				/>
+			</div>
+			<div className="card ">
+				<img
+					src="https://emilia.lekoarts.de/static/30dbf6c572d154ccfe746dc0d66793c9/80926/sean-foley-0JD7kvxAq0Y-unsplash.webp"
+					className="card-img-top"
+					alt="..."
+				/>
+			</div>
+			<div className="card ">
+				<img
+					src="https://upload.wikimedia.org/wikipedia/en/1/19/Batman_%28circa_2016%29.png"
+					className="card-img-top"
+					alt="..."
+				/>
+			</div>
+			<div className="card">
+				<img
+					src="https://emilia.lekoarts.de/static/3e2691a6f97efd3c87ea50a3091a7ce1/ee7d3/allef-vinicius-_H0siQHdMM4-unsplash.webp"
+					className="card-img-top"
+					alt="..."
+				/>
+			</div>
+		</div>
 	</div>
 );
+
+{
+	/* <div classNameName="text-center mt-5">
+	<h1>AnmaProductions</h1>
+	<a href="https://www.instagram.com/anmaproductions/">
+		<p>Instagram</p>
+	</a>
+
+	<div classNameName="row">
+		<div classNameName="col-sm-6">
+			<img
+				src="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/88004731_226442865190024_7356238380486623232_n.jpg?_nc_cat=108&_nc_oc=AQnvBg9nRUyDYcg22drKEc8e6IpVjV8IFJ_n47UgXG6lgTap_GhfwJECh5aMDO03qzyJggvCylY8CAEteftul8LL&_nc_ht=scontent.fmia1-1.fna&oh=b577bf6936c7f5db4e61b53ccd3b0ac7&oe=5F030C6E"
+				height="800"
+				width="70%"
+				classNameName="img-fluid height-1000 max-width-100%"
+				alt="..."
+			/>
+		</div>
+		<div classNameName="col-4">
+			<img
+				src="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/88004731_226442865190024_7356238380486623232_n.jpg?_nc_cat=108&_nc_oc=AQnvBg9nRUyDYcg22drKEc8e6IpVjV8IFJ_n47UgXG6lgTap_GhfwJECh5aMDO03qzyJggvCylY8CAEteftul8LL&_nc_ht=scontent.fmia1-1.fna&oh=b577bf6936c7f5db4e61b53ccd3b0ac7&oe=5F030C6E"
+				height="800"
+				width="100%"
+				classNameName="img-fluid height-auto max-width-100%"
+				alt="..."
+			/>
+		</div>
+		<div classNameName="col-4">
+			<img
+				src="https://scontent.fmia1-1.fna.fbcdn.net/v/t1.15752-9/88004731_226442865190024_7356238380486623232_n.jpg?_nc_cat=108&_nc_oc=AQnvBg9nRUyDYcg22drKEc8e6IpVjV8IFJ_n47UgXG6lgTap_GhfwJECh5aMDO03qzyJggvCylY8CAEteftul8LL&_nc_ht=scontent.fmia1-1.fna&oh=b577bf6936c7f5db4e61b53ccd3b0ac7&oe=5F030C6E"
+				height="800"
+				width="100%"
+				classNameName="img-fluid height-auto max-width-100%"
+				alt="..."
+			/>
+		</div>
+	</div>
+</div> */
+}
